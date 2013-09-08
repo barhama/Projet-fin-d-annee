88fbd230228f7d57bdc6840f6a48cfc3f231c758
barhama <khayrilahi@gmail.com>
2013-09-08 02:08:07 +0000

    Update facture1.c
    VOILA NOTRE WORK.C DE CETTE SEMAINE

--- gestionCommande/facture1.c
+++ gestionCommande/facture1.c
@@ -1,11 +1,42 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-struct clt
-{
-       char* idclient;
-       char* nom;
-       char* prenom;
-} client;
-struct cmd
-{
+
+#include <stdio.h>
+#include <stdlib.h>
+   
+        typedef struct clt
+         {
+                int code_client;
+                char* nom;
+                char* prenom;
+                char* adresse;
+         }c,c1;
+      
+         typedef struct cmd
+         {
+              struct clt code_client;  
+                int qualite;
+                float prixunit;
+                
+         } com;
+   
+       typedef struct fture
+         {
+               struct clt code_client;
+               float montant;
+         } fact;
+        
+              
+         int main()
+         {
+              
+              
+               struct clt c,c1;
+               struct cmd com;
+               struct fture fact;
+               c.code_client=1;
+               c.nom="my name\n";
+               system("pause");	
+               return 0;
+         }
